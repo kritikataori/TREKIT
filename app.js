14928f8cdf9d12d8ee2aba9c40752d1689a624ee
kritikataori <taorikritika@gmail.com>
2024-08-13 00:54:20 +0530

    Adding Basic Styles

--- app.js
+++ app.js
@@ -20,30 +20,34 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+//home page
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+//index page
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
 })
 
+//new campground form
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
+//post a new campground
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+//individual show page for each campground
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
     try {
         const campground = await Campground.findById(id)
-        console.log('Campground:', campground);  // Log the data
         if (!campground) {
             return res.status(404).send('Campground not found');
         }
@@ -54,17 +58,20 @@ app.get('/campgrounds/:id', async (req, res) => {
     }
 })
 
+//edit form for a campground
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
     res.render('campgrounds/edit', { campground })
 })
 
+//edit the campground
 app.put('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground }, { new: true }) //... is spread operator
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+//delete a campground
 app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
