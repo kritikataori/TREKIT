97bcce67ee0f7d9750d036de61ce0e18e5af4435
kritikataori <taorikritika@gmail.com>
2024-08-17 22:54:23 +0530

    Basic Error Handling

---
+++ schemas.js
@@ -0,0 +1,15 @@
+const Joi = require('joi')
+
+//Joi.object({...}): This creates a Joi schema object, which specifies the structure of the data you expect.
+
+// campground: Joi.object({...}): This indicates that the req.body should contain an object called campground. Inside this object, there are several required fields.
+
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+})
