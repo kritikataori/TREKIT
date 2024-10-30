4eb0e414e3f79c427aed64a6d126394e7bf3db2f
kritikataori <taorikritika@gmail.com>
2024-10-30 23:25:19 +0530

    basic security measures

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+
 const Campground = require('./models/campground')
 const Review = require('./models/review')
 const User = require('./models/user')
@@ -40,12 +43,67 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(mongoSanitize({replaceWith: '_'}))
+
+app.use(helmet())
+    const scriptSrcUrls = [
+        "https://stackpath.bootstrapcdn.com/",
+        // "https://api.tiles.mapbox.com/",
+        // "https://api.mapbox.com/",
+        "https://kit.fontawesome.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://cdn.jsdelivr.net",
+        "https://cdn.maptiler.com/", 
+    ];
+    const styleSrcUrls = [
+        "https://kit-free.fontawesome.com/",
+        "https://stackpath.bootstrapcdn.com/",
+        // "https://api.mapbox.com/",
+        // "https://api.tiles.mapbox.com/",
+        "https://fonts.googleapis.com/",
+        "https://use.fontawesome.com/",
+        "https://cdn.jsdelivr.net",
+        "https://cdn.maptiler.com/", 
+    ];
+    const connectSrcUrls = [
+        // "https://api.mapbox.com/",
+        // "https://a.tiles.mapbox.com/",
+        // "https://b.tiles.mapbox.com/",
+        // "https://events.mapbox.com/",
+        "https://api.maptiler.com/", 
+    ];
+    const fontSrcUrls = [];
+    app.use(
+        helmet.contentSecurityPolicy({
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    "https://api.maptiler.com/", 
+                    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+                    "https://images.unsplash.com/",
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+            },
+        })
+    );
+
+
 const sessionConfig = {
     secret: 'thisismysecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
+        name: 'session',
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
--- schemas.js
+++ schemas.js
@@ -1,4 +1,27 @@
-const Joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(), //extension defined ON joi.string()
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [], //nothing is allowed
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value }) //if the returned value does not equal to the original value, then that means something was removed and we call the string.escapeHTML to display the message
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension) //to call the extension on joi
 
 //Joi.object({...}): This creates a Joi schema object, which specifies the structure of the data you expect.
 
@@ -6,18 +29,18 @@ const Joi = require('joi')
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         //image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 })
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5)
     }).required()
 })
